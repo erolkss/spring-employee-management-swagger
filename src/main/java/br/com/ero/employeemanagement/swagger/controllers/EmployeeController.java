e97e5c9fab4772a061c38c64c3d7e01a62cc9a9d
Lucas Lima <lucaserolima@gmail.com>
2024-04-11 20:35:45 -0400

    feat: Add method to create employees.

--- src/main/java/br/com/ero/employeemanagement/swagger/controllers/EmployeeController.java
+++ src/main/java/br/com/ero/employeemanagement/swagger/controllers/EmployeeController.java
@@ -1,7 +1,12 @@
 package br.com.ero.employeemanagement.swagger.controllers;
 
+import br.com.ero.employeemanagement.swagger.models.Employee;
 import br.com.ero.employeemanagement.swagger.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    @PostMapping("/employees")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
 
 
 }
--- src/main/java/br/com/ero/employeemanagement/swagger/services/EmployeeService.java
+++ src/main/java/br/com/ero/employeemanagement/swagger/services/EmployeeService.java
@@ -1,5 +1,6 @@
 package br.com.ero.employeemanagement.swagger.services;
 
+import br.com.ero.employeemanagement.swagger.models.Employee;
 import br.com.ero.employeemanagement.swagger.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
