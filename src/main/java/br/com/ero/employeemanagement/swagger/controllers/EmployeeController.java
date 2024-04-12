d471ba694d369b82a0ff052764efe2c4881f872d
Lucas Lima <lucaserolima@gmail.com>
2024-04-11 20:38:48 -0400

    feat: Add method to get all employees.

--- src/main/java/br/com/ero/employeemanagement/swagger/controllers/EmployeeController.java
+++ src/main/java/br/com/ero/employeemanagement/swagger/controllers/EmployeeController.java
@@ -5,10 +5,9 @@ import br.com.ero.employeemanagement.swagger.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +19,12 @@ public class EmployeeController {
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         Employee newEmployee = employeeService.addEmployee(employee);
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
 
--- src/main/java/br/com/ero/employeemanagement/swagger/services/EmployeeService.java
+++ src/main/java/br/com/ero/employeemanagement/swagger/services/EmployeeService.java
@@ -5,6 +5,8 @@ import br.com.ero.employeemanagement.swagger.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -13,5 +15,9 @@ public class EmployeeService {
 
     public Employee addEmployee(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
     }
 }
