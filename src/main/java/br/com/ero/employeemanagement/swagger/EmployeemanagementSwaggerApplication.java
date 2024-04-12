8ff75cd08bd06fa604091f217a86fdcb515be48b
Lucas Lima <lucaserolima@gmail.com>
2024-04-11 20:16:42 -0400

    First Commit - Create Project and connection with database

---
+++ src/main/java/br/com/ero/employeemanagement/swagger/EmployeemanagementSwaggerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.employeemanagement.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagementSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ero/employeemanagement/swagger/EmployeemanagementSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.employeemanagement.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
